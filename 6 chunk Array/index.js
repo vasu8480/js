2e588fff15a33cd3a8f39ae97982bcb313c6c7f5
vasu8480 <vasu8480@gmail.com>
2022-07-19 19:14:12 +0530

    using terminal vscode

---
+++ 6 chunk Array/index.js
@@ -0,0 +1,11 @@
+//---------------------------------------------------------- method-1 ---------------------------------------------------------- 
+function chunk(arr,size){
+	const chunked=[];
+	let index=0;
+	while (index <arr.length){
+		chunked.push(arr.slice(index,index+size));
+		index+=size;
+	}
+	return chunked;
+}
+console.log(chunk([1,2,3,4,5,6,7,8],3));
