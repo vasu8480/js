7ae273ba1744e1c30a62dabd4a41861533611ff1
vasu8480 <vasu8480@gmail.com>
2022-07-19 19:15:21 +0530

    using terminal vscode

--- ro.js
+++ ro.js
@@ -1,19 +1,13 @@
 //---------------------------------------------------------- method-1 ---------------------------------------------------------- 
-function fizzbuzz(n){
-	const charmap={};
-	let max=0;
-	let maxChar='';
-
-	for (let char of n){
-		charmap[char]=charmap[char]+1 || 1; 
+function chunk(arr,size){
+	const chunked=[];
+	for(let ele of arr){
+		const last=chunked[chunked.length-1];
+		if(!last || last.length===size)
+			chunked.push([ele]);
+		else
+			last.push(ele);
 	}
-
-	for(let char in charmap){
-		if(charmap[char]>max){
-			max=charmap[char];
-			maxChar=char;
-		}
-	}
-	return maxChar;
+	return chunked;
 }
-console.log(fizzbuzz("aab111215sfd45454545484b"));
+console.log(chunk([1,2,3,4,5,6,7,8],3));
