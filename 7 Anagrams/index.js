2ebe6fd38d1ac8d590e3d382c860bca956a9c1c1
vasu8480 <vasu8480@gmail.com>
2022-07-20 10:31:02 +0530

    using terminal vscode

--- 7 Anagrams/index.js
+++ 7 Anagrams/index.js
@@ -1,4 +1,4 @@
-//---------------------------------------------------------- method-2 ---------------------------------------------------------- 
+//---------------------------------------------------------- method-1 ---------------------------------------------------------- 
 function anagrams(str1,str2){
 	return remove_spaces_quotes(str1)===remove_spaces_quotes(str2);
 }
@@ -9,5 +9,28 @@ function remove_spaces_quotes(str){
 	.sort() 
 	.join('');			// changing to string
 			
+}
+console.log(anagrams('hi! vasu','hi Vasu'));
+//---------------------------------------------------------- method-2 ---------------------------------------------------------- 
+function anagrams(str1,str2){
+	const acharmap=remove_spaces_quotes(str1);
+	const bcharmap=remove_spaces_quotes(str2);
+
+	if (Object.keys(acharmap).length !== Object.keys(bcharmap).length)
+			return false;
+
+	for(let char in acharmap){
+		if (acharmap[char] !== bcharmap[char])
+				return false;
+	}
+	return true;
+}
+function remove_spaces_quotes(str){
+	const charmap={};
+
+	for(let char of str.replace(/[^\w]/g,'').toLowerCase())
+			charmap[char]=charmap[char]+1 || 1;
+
+	return charmap;
 }
 console.log(anagrams('hi! vasu','hi Vasu'));
