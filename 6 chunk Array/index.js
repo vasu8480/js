83a644d5e5186ee49c0cfc58f240692c47be9dff
vasu8480 <vasu8480@gmail.com>
2022-07-20 11:00:49 +0530

    using terminal vscode

--- 6 chunk Array/index.js
+++ 6 chunk Array/index.js
@@ -9,3 +9,16 @@ function chunk(arr,size){
 	return chunked;
 }
 console.log(chunk([1,2,3,4,5,6,7,8],3));
+//---------------------------------------------------------- method-2 ---------------------------------------------------------- 
+function chunk(arr,size){
+	const chunked=[];
+	for(let ele of arr){
+		const last=chunked[chunked.length-1];
+		if(!last || last.length===size)
+			chunked.push([ele]);
+		else
+			last.push(ele);
+	}
+	return chunked;
+}
+console.log(chunk([1,2,3,4,5,6,7,8],3));
--- ro.js
+++ ro.js
@@ -1,13 +1,9 @@
 //---------------------------------------------------------- method-1 ---------------------------------------------------------- 
-function chunk(arr,size){
-	const chunked=[];
-	for(let ele of arr){
-		const last=chunked[chunked.length-1];
-		if(!last || last.length===size)
-			chunked.push([ele]);
-		else
-			last.push(ele);
-	}
-	return chunked;
+function word_capitalize(str){
+	const words=[];
+
+	for(let word of str.split(' '))
+			words.push(word[0].toUpperCase()+word.slice(1));
+	return words.join(" ");
 }
-console.log(chunk([1,2,3,4,5,6,7,8],3));
+console.log(word_capitalize('hi! vasu iam sham calling you'));asu'));
