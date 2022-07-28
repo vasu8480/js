fdf3d616ec70c363ff83ccf86a28c078793781f3
vasu8480 <vasu8480@gmail.com>
2022-07-28 15:29:27 +0530

    new file:   2 Data Structures/1 Arrays/1.index.js 	new file:   2 Data Structures/1 Arrays/2 index.js 	deleted:    2 Data Structures/index.js

---
+++ 2 Data Structures/1 Arrays/1.index.js
@@ -0,0 +1,13 @@
+const str=['a','b','c','d'];
+str.push('e');
+console.log(str);
+
+str.pop();
+str.pop();
+console.log(str);
+
+str.unshift('x');
+console.log(str);
+
+str.splice(2,0,'z');
+console.log(str);
