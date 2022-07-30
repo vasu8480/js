b21001827aae0a0bf95880b12aa0586c385b7ae2
vasu8480 <vasu8480@gmail.com>
2022-07-30 12:46:27 +0530

    asd

---
+++ 2 Data Structures/3 LinkedLists/3 Doubly LL.js
@@ -0,0 +1,81 @@
+class Node{
+	constructor(data){
+			this.data = data;
+			this.next = null;
+			this.prev = null;
+	}
+}
+
+class Doubly_LinkedList{
+constructor(value){
+	this.head = {
+		value: value,
+		next: null,
+		prev: null
+	}
+
+	this.tail = this.head;
+	this.length = 1;
+}
+append(value){
+	const newNode = new Node(value);
+	newNode.prev = this.tail; 
+	this.tail.next = newNode;
+	this.tail = newNode;
+	this.length++;
+	return this;
+}
+prepend(value){
+	const newNode = new Node(value);
+	newNode.next = this.head;
+	this.head.prev = newNode;
+	this.head = newNode;
+	this.length++;
+	return this;
+}
+printList(){
+	const array = [];
+	let currentNode = this.head;
+	while(currentNode!==null){
+		array.push(currentNode.value);
+		currentNode = currentNode.next;
+	}
+	return array;
+}
+insert(index, value){
+	if(index >= this.length){
+		return this.append(value);
+	}
+	const newNode = new Node(value);
+	const leader = this.traverseToIndex(index - 1);
+	const follower = leader.next;
+	leader.next = newNode;
+	newNode
+	newNode.next = follower;
+	this.length++;
+	return this;
+}
+traverseToIndex(index){
+	let counter = 0;
+	let currentNode = this.head;
+	while(counter !== index){
+		currentNode = currentNode.next;
+		counter++;
+	}
+	return currentNode;
+}
+remove(index){
+	const leader = this.traverseToIndex(index - 1);
+	const unwantedNode = leader.next;
+	leader.next = unwantedNode.next;
+	this.length--;
+	return this;
+}
+}
+const myLinkedList = new Doubly_LinkedList(10);
+myLinkedList.append(45);
+myLinkedList.append(16)
+myLinkedList.prepend(5);
+myLinkedList.printList();
+myLinkedList.insert(2,152);
+console.log(myLinkedList);
