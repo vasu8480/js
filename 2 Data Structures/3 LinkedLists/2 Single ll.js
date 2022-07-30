972af018d79e9426f840a119081789328a89e41a
vasu8480 <vasu8480@gmail.com>
2022-07-30 12:58:38 +0530

    lll

--- 2 Data Structures/3 LinkedLists/2 Single ll.js
+++ 2 Data Structures/3 LinkedLists/2 Single ll.js
@@ -64,12 +64,29 @@ class LinkedList{
 		this.length--;
 		return this;
 	}
+	reverse(){
+		if(!this.head.next){
+			return this.head;
+		}
+		let first = this.head;
+		this.tail = this.head;
+		let second = first.next;
+		let temp;
+		while(second){
+			temp = second.next;
+			second.next = first;
+			first = second;
+			second = temp;
+		}
+		this.head.next = null;
+		this.head = first;
+		return this;
+	}
 }
-const myLinkedList = new Doubly_LinkedList(10);
+const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
-myLinkedList.printList();
 myLinkedList.insert(1, 99);
-myLinkedList.remove(1);
+myLinkedList.reverse();
 myLinkedList.printList();
 console.log(myLinkedList);
