50eedaa47bb4c3c09646d2cde657d74b2608631c
vasu8480 <vasu8480@gmail.com>
2022-08-08 11:21:57 +0530

    express js

--- nodejs/5 express js/server.js
+++ nodejs/5 express js/server.js
@@ -1,16 +1,5 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-	console.log('request was made: ' + req.url);
-	//console.log(req.headers);
-	console.log(req.method);
-	const user = {
-		name: 'John',
-		age: 30
-	};
+const app = express();
 
-	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify(user));
-})
-
-server.listen(3000);
+app.listen(3000);
