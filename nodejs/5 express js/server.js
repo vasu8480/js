429d70183bad2bfb9f24f881f4d3f3a2ab291e8a
vasu8480 <vasu8480@gmail.com>
2022-08-08 11:40:48 +0530

    nodemon

--- nodejs/5 express js/server.js
+++ nodejs/5 express js/server.js
@@ -2,4 +2,20 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.get('/users', (req, res) => {
+	res.send("geeting");
+});
+
+app.post('/users', (req, res) => {
+	const user = {
+		name: 'John',
+		age: 30
+	}
+	res.send('user');
+});
+
 app.listen(3000);
