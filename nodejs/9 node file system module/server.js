257dc00f61cdc356a13ef676b0308cd653d9016f
vasu8480 <vasu8480@gmail.com>
2022-08-08 14:37:19 +0530

    modified:   nodejs/9 node file system module/hello.txt 	modified:   nodejs/9 node file system module/server.js

--- nodejs/9 node file system module/server.js
+++ nodejs/9 node file system module/server.js
@@ -11,3 +11,33 @@ fs.readFile('./hello.txt',(err,data) =>
 ///-------------method-2 fast way to read file
 const file=fs.readFileSync('./hello.txt');
 console.log('Sync',file.toString());
+
+//Append file
+fs.appendFile('./hello.txt','\nHello World',(err) =>
+{
+	if(err){
+		console.log(err);
+	}
+	console.log('File Appended');
+}
+);
+
+//write file
+fs.writeFile('./hello.txt','Hello World',(err) =>
+{
+	if(err){
+		console.log(err);
+	}
+	console.log('File Written');
+}
+);
+
+//delete file
+fs.unlink('./bye.txt',(err) =>
+{
+	if(err){
+		console.log(err);
+	}
+	console.log('File Deleted');
+}
+);
