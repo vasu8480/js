e0404d573dacc731ff7b402db7f632e93e8c0248
Vasu Nurukurthi <Vasu8480@gmail.com>
2022-09-23 10:46:29 +0530

    Create callback.js

---
+++ namaste_js/callback.js
@@ -0,0 +1,13 @@
+//callback function					 	used in event listener
+setTimeout(function(){
+	console.log("timer");
+},3000);
+
+function x(y){
+	console.log("x");
+	y();
+}
+x(function y(){
+	console.log("y");
+});
+
